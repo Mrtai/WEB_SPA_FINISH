9f4b7052dd08b0bc70477b4364e4a9c4dddc941e
Vantam1601 <tam16011998@gmail.com>
2019-11-20 09:37:27 +0700

    comit masterpage

--- SpaWeb/Demo.aspx.designer.cs
+++ SpaWeb/Demo.aspx.designer.cs
@@ -10,15 +10,24 @@
 namespace SpaWeb {
     
     
-    public partial class _Default {
+    public partial class Demo {
         
         /// <summary>
-        /// Repeater1 control.
+        /// Button1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Calendar1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
     }
 }
